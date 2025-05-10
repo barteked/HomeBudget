b8236520bccbcbb43e82ef5b3d4e0ada189204a6
Bartek <bartekkedra@onet.pl>
2025-05-10 14:24:02 +0200

    add CategoryService

---
+++ src/main/java/pl/kedrabartosz/HomeBudget/service/CategoryService.java
@@ -0,0 +1,31 @@
+package pl.kedrabartosz.HomeBudget.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.kedrabartosz.HomeBudget.Category;
+import pl.kedrabartosz.HomeBudget.repository.CategoryRepository;
+
+@Service
+//service sie dodaje dla springa! nie jest to koniecznie bez tego tez by smigalo :D!
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService( @Autowired CategoryRepository categoryRepository) {
+        // tu nastepuje wstrzykneicie zaleznosci!
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category saveCategory(String name) {
+        return categoryRepository.save(name);
+    }
+
+    public Category updateCategory(String oldName, String newName) {
+        return categoryRepository.update(oldName, newName);
+    }
+
+    public Category getCategory(String name) {
+        return categoryRepository.getCategory(name);
+    }
+
+}
