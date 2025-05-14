f55e29b8e81e0a928e7b2f9ef85187d853462adb
Bartek <bartekkedra@onet.pl>
2025-05-14 22:18:59 +0200

    Cost service supplement

--- src/main/java/pl/kedrabartosz/HomeBudget/service/CostService.java
+++ src/main/java/pl/kedrabartosz/HomeBudget/service/CostService.java
@@ -7,17 +7,35 @@ import pl.kedrabartosz.HomeBudget.Category;
 import pl.kedrabartosz.HomeBudget.Cost;
 import pl.kedrabartosz.HomeBudget.repository.CostRepository;
 
+import java.util.List;
+
 @Service
 public class CostService {
 
-  private CostRepository listBasedRepository;
+    private CostRepository listBasedRepository;
 
-  public CostService(@Autowired CostRepository listBasedRepository) {
-    // = to przypisanie = to jest włąśnie wstrzykiwanie zależności
-    this.listBasedRepository = listBasedRepository;
-  }
+    public CostService(@Autowired CostRepository listBasedRepository) {
+        // = to przypisanie = to jest włąśnie wstrzykiwanie zależności
+        this.listBasedRepository = listBasedRepository;
+    }
 
-  public Cost saveCost(String name, double price, Category category){
-    return listBasedRepository.addCost(name, price,category);
-  }
+    public Cost saveCost(String name, double price, Category category) {
+        return listBasedRepository.addCost(name, price, category);
+    }
+
+    public Cost updateCost(String oldProduct, String newProduct, double newPrice) {
+        return listBasedRepository.updateCost(oldProduct, newProduct, newPrice);
+    }
+
+    public Cost getCost(String product) {
+        return listBasedRepository.getCost(product);
+    }
+
+    public Cost deleteCost(String product) {
+        return listBasedRepository.deleteCost(product);
+    }
+
+    public List<Cost> getAllCosts() {
+        return listBasedRepository.getAll();
+    }
 }
