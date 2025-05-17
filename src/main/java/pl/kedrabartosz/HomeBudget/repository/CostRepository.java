47378360448bae948474d8c39eb5e4a40aa1c011
Bartek <bartekkedra@onet.pl>
2025-05-17 14:51:19 +0200

    Method refactoring

--- src/main/java/pl/kedrabartosz/HomeBudget/repository/CostRepository.java
+++ src/main/java/pl/kedrabartosz/HomeBudget/repository/CostRepository.java
@@ -4,15 +4,16 @@ import pl.kedrabartosz.HomeBudget.Category;
 import pl.kedrabartosz.HomeBudget.Cost;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CostRepository {
     Cost addCost(String product, double price, Category category);
 
-    public Cost updateCost(String oldProduct, String newProduct, double newPrice);
+    public Optional<Cost> updateCost(String oldProduct, String newProduct, double newPrice);
 
-    public Cost getCost(String product);
+    public Optional<Cost> getCost(String product);
 
-    public Cost deleteCost(String product);
+    public Optional<Cost> deleteCost(String product);
 
     public List<Cost> getAll();
 }
