c8d262a0535641727a07c3c49eb197da22dca98c
Bartek <bartekkedra@onet.pl>
2025-05-21 19:08:19 +0200

    add JUnit5 in ListBasedCostRepositoryTest

---
+++ src/test/java/pl/kedrabartosz/HomeBudget/repository/ListBasedCostRepositoryTest.java
@@ -0,0 +1,31 @@
+package pl.kedrabartosz.HomeBudget.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.kedrabartosz.HomeBudget.Category;
+import pl.kedrabartosz.HomeBudget.Cost;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListBasedCostRepositoryTest {
+
+    private ListBasedCostRepository listBasedCostRepository = new ListBasedCostRepository(new ArrayList<>());
+    @Test
+    public void shouldAddNewCost(){
+        //Given to co daje przygotowuje dane
+        String product = "product";
+        double price = 22;
+        Category category = Category.builder().build();
+        //When co testujemy 1 max 2 linijki!!
+        Cost actual = listBasedCostRepository.addCost(product, price, category);
+
+        //Then sprawdzam wynik z when!
+        Assertions.assertEquals(product, actual.getProduct());
+        Assertions.assertEquals(price,actual.getPrice());
+        Assertions.assertEquals(category,actual.getCategory());
+    }
+
+}
