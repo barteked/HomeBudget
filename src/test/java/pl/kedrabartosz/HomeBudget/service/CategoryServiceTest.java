4e13cae845a6da142d091eb22edde1f2e999449d
Bartek <bartekkedra@onet.pl>
2025-05-28 13:40:45 +0200

    add Mock in CategorySericeTest

--- src/test/java/pl/kedrabartosz/HomeBudget/service/CategoryServiceTest.java
+++ src/test/java/pl/kedrabartosz/HomeBudget/service/CategoryServiceTest.java
@@ -3,26 +3,37 @@ package pl.kedrabartosz.HomeBudget.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import pl.kedrabartosz.HomeBudget.Category;
 import pl.kedrabartosz.HomeBudget.repository.CategoryRepository;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 import static pl.kedrabartosz.HomeBudget.service.MyTestCategoryRepository.CATEGORY_NAME;
 
 class CategoryServiceTest {
     private CategoryService categoryService;
+    @Mock
+    private CategoryRepository myTestCategoryRepository;
 
     @BeforeEach
     public void setUp() {
-        CategoryRepository myTestCategoryRepository = new MyTestCategoryRepository();
+        MockitoAnnotations.openMocks(this);
         categoryService = new CategoryService(myTestCategoryRepository);
     }
 
     @Test
     public void shouldGetCategory() {
         //given
-
+        Category category = new Category(CATEGORY_NAME);
+        Optional<Category> getCategoryOptional = Optional.of(category);
+        when(myTestCategoryRepository.getCategory(eq("Bartek"))).thenReturn(getCategoryOptional);
         String name = "Bartek";
+
         //when
         Category actual = categoryService.getCategory(name);
         //then
