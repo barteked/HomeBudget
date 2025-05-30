6f296312a4a99b371211c7914c7161a711d7ea80
Bartek <bartekkedra@onet.pl>
2025-05-30 16:27:24 +0200

    adding comments to the code

--- src/main/java/pl/kedrabartosz/HomeBudget/repository/FileBasedCategoryRepository.java
+++ src/main/java/pl/kedrabartosz/HomeBudget/repository/FileBasedCategoryRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 public class FileBasedCategoryRepository implements CategoryRepository {
 
-    private static final String CSV_PATH = "categories.csv";
+    private static final String CSV_PATH = "categories.csv";// sciezka do pliku
 
     @Override
     public Category save(String name) {
@@ -39,31 +39,32 @@ public class FileBasedCategoryRepository implements CategoryRepository {
     @Override
     public List<Category> getAll() {
         List<Category> categoryList = new ArrayList<>();
-        BufferedReader reader = null;
-
-        try {
-            Path path = Paths.get(
-                    getClass().getClassLoader()
-                            .getResource(CSV_PATH)
-                            .toURI()
+        BufferedReader reader = null;// buforowany czytnik do odczytu pliku linia po linii!
+        // przygotowanie takiego pudelka zeby przechowywalo czytnik, jak nie zdefiniuje nulla to wywali blad!
+        try {// zlokalizowanie pliku csv w classpath i konwersja na obiekt path!
+            Path path = Paths.get(// na podstawie tego URI tworzymy obiekt Path ktory mowi gdzie w systemie plikow znajduje sie ten plik!
+                    getClass().getClassLoader()// zwraca to plik ktory wie jak ladowac pliki i klasy z zasobow classpath
+                            .getResource(CSV_PATH)// to znajduje i odczytuje plik w katalogu resources!
+                            .toURI()//zamieniamy ten adres URL na obiekt typu URI, czyli ustandaryzowaną reprezentację sciezki
             );
-            reader = Files.newBufferedReader(path);
+            reader = Files.newBufferedReader(path);// otwiera to plik wskazany przez path i zwraca obiekt BufferedReader
+            // i dzieki temu cala zawartosc mozna czytac linia po linii. Nie trzeba recznie tym zarzadzac!
 
-            String line;
+            String line;// petla odczytujaca kolejne linie az do konca pliku!
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
-                String[] tokens = line.split(",");
-                int id = Integer.parseInt(tokens[0].trim());
-                String name = tokens[1].trim();
-                categoryList.add(new Category(id, name));
+                String[] tokens = line.split(",");// oddzielnie przecinkiem na kolumny id i name!
+                int id = Integer.parseInt(tokens[0].trim());// parsowanie czyli konwertowanie, pierwszego tokenu na identyfiaktor typu int!
+                String name = tokens[1].trim();// drugi token to nazwa kategorii!
+                categoryList.add(new Category(id, name));//utworzenie obiektu category i dodanie go do listy
             }
 
         } catch (IOException | URISyntaxException e) {
-            System.err.println("Error reading CSV file: " + e.getMessage());
+            System.err.println("Error reading CSV file: " + e.getMessage());// obsluga bledow wej/wyj
         } finally {
             if (reader != null) {
                 try {
-                    reader.close();
+                    reader.close();// zamkniecie czytnika zeby zwolnic zasoby systemowe to wazne!!
                 } catch (IOException ex) {
                     System.err.println("Error closing reader: " + ex.getMessage());
                 }
