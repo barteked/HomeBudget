41dd7354bd7d123bf0e36b7f243e1a19dab4d2b0
Bartek <bartekkedra@onet.pl>
2025-06-15 20:52:04 +0200

    add implement list-based repository & service for ShoppingCart

--- src/main/java/pl/kedrabartosz/HomeBudget/service/ShoppingCartService.java
+++ src/main/java/pl/kedrabartosz/HomeBudget/service/ShoppingCartService.java
@@ -1,4 +1,21 @@
 package pl.kedrabartosz.HomeBudget.service;
 
+import pl.kedrabartosz.HomeBudget.Cost;
+import pl.kedrabartosz.HomeBudget.Person;
+import pl.kedrabartosz.HomeBudget.ShoppingCart;
+
+import java.util.Optional;
+
 public interface ShoppingCartService {
+
+    Cost addExpense(Person person, String product, double price, String categoryName);
+
+    Optional<Cost> updateExpense(Person person,
+                                 String oldProduct,
+                                 String newProduct,
+                                 double newPrice);
+
+    Optional<Cost> removeExpense(Person person, String product);
+
+    ShoppingCart getCart(Person person);
 }
